55b756185af14cebffc0fe6d1416552de52a340e
oscurantismo <lubyatowo@gmail.com>
2025-05-02 22:04:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ from routes.debug_tools.reward_logs import reward_logs_bp
 from routes.tasks import tasks_routes
 from routes.notifications import notifications_routes
 from routes.debug_tools.subscriptions import subscription_routes
-from routes.debug_tools.subscriptions import register_logging_routes
+from routes.debug_tools import register_logging_routes
 
 # ✅ Log startup
 log_event("🚀 Initializing Flask app")
